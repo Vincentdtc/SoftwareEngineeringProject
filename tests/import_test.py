1b08fd220f25fc3579bdfc6e2721ca06d0d2531c
Vincent Kruit <vincent.kruit@dtc.ox.ac.uk>
2024-10-31 10:29:40 +0000

    test

--- tests/import_test.py
+++ tests/import_test.py
@@ -1,10 +1,19 @@
 import unittest
+import pandas as pd
 import os
-import sys
+#import sys
 
-sys.path.insert(1,os.getcwd())
-from scripts.import_data import parse_data
+#sys.path.insert(1,os.getcwd())
+#from scripts.import_data import parse_data
 
+def parse_data(path='Experimental_data'):
+    dict = {}
+    dir = os.listdir(path)
+    for filename in dir:
+        print(path + '/' + filename)
+        data = pd.read_excel(path + '/' + filename)
+        dict[filename] = data
+    return dict
 class TestImport(unittest.TestCase):
     def test_type(self):
         """Test that parse_data returns dict type"""
