4c6af3cf42184e865311012cd0fcbf17746a5c63
Vincent Kruit <vincent.kruit@dtc.ox.ac.uk>
2024-10-31 15:11:06 +0000

    test

--- VKscripts/tests/test_process.py
+++ VKscripts/tests/test_process.py
@@ -5,4 +5,5 @@ class TestImport(unittest.TestCase):
 
     def test_type(self):
         """Test that parse_data returns dict type"""
-        self.assertTrue(isinstance(vk.Process_data.process_data(), dict))
+        input = vk.import_data.parse_data()
+        self.assertTrue(isinstance(vk.Process_data.process_data(input), dict))
