cc9d25addd2522b74158f615550958dc44e5cdf4
Vincent Kruit <vincent.kruit@dtc.ox.ac.uk>
2024-10-31 15:26:56 +0000

    test

--- VKscripts/tests/test_visualise.py
+++ VKscripts/tests/test_visualise.py
@@ -7,4 +7,4 @@ class TestImport3(unittest.TestCase):
         """Test that parse_data returns dict type"""
         input = vk.import_data.parse_data()
         data = vk.Process_data.process_data(input)
-        self.assertTrue(isinstance(vk.visualise.create_fig(data), None))
+        self.assertTrue(isinstance(vk.visualise.create_fig(data), type(None)))
